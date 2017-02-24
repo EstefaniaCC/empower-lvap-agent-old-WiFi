072e0617e62b82c7f616997bb98247f3c55b2095
EstefaniaCC <estefi.coronado@gmail.com>
2017-02-24 19:02:27 +0100

    merge commit

--- elements/empower/empowerlvapmanager.hh
+++ elements/empower/empowerlvapmanager.hh
@@ -255,11 +255,8 @@ public:
 	int handle_add_busyness_trigger(Packet *, uint32_t);
 	int handle_del_busyness_trigger(Packet *, uint32_t);
 	int handle_busyness_request(Packet *, uint32_t);
-<<<<<<< HEAD
 	int handle_incom_mcast_addr_response(Packet *p, uint32_t offset);
-=======
 	int handle_wtp_counters_request(Packet *, uint32_t);
->>>>>>> upstream/master
 
 	void send_hello();
 	void send_probe_request(EtherAddress, String, EtherAddress, int, empower_bands_types);
